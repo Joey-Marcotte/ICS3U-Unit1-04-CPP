5a7c837fd0b937c9c719eb683701ea224b14480a
Ubuntu <ubuntu@ip-172-31-27-176.ec2.internal>
2019-09-13 17:27:31 +0000

    basic math program

---
+++ basic_math.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2019 Joey Marcotte All rights reserved
+//
+// Created by: Joey Marcotte
+// Created on: September 2019
+// This program does basic math
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "If a Rectangle has dimensions: " << std::endl;
+    std::cout << "5mm x 3mm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is =mm^2" << (5*3) << std::endl;
+    std::cout << "Perimeter is =mm" << (2*(5+3)) << std::endl;
+}
